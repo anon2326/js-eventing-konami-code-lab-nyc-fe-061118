ad288eb25669abd783418f3600c7d8dc043c566a
Jeffrey Jiang <anon0503@gmail.com>
2018-08-10 03:00:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
-function init() {
-  // Write your JavaScript code inside the init() function
+document.body.addEventListener("keydown", init)
 
-}
+function init(event) {
+  debugger;
+  let keys = [];
+  let key = event.which;
+  if (key == code[index]){
+    index++;
+    if(index === code.length){
+      alert("YOU DID IT MAN, WE BELIVED IN YOU (obviously, I mean who wouldn't.... right?)");
+    }
+  }
+  else if {
+    index = 0;
+  }
